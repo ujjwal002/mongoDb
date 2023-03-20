1f7abd1a30a7d3f1c52ba275f31529366377eeff
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-21 00:10:28 +0530

    collection created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ttchannel");
+  await mongoose.connect("mongodb://127.0.0.1:27017/thapatechical");
   console.log("connection succesfull");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
@@ -27,3 +27,13 @@ const playlistSchema = mongoose.Schema({
     default: Date.now,
   },
 });
+
+// a mongoose model is a wrapper on the mongoose schema .
+// a mongoosae schema defines the structure of the documents
+// default value ,validation etc wheras a mogoose module
+// provide an interface to the databse for creating ,querying ,delteing records
+// throgh this mongoose model you can create CRUD operation
+
+// model ka create karna means collectionn ko create karan hota hai mean you can create tables
+// collection creation
+const Playlist = new mongoose.model("Playlist", playlistSchema);
